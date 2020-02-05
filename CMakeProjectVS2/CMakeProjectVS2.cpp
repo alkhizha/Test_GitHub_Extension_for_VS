edb3199781e30f2c980a4a2a5b55c43d6a79fbdd
alkhizha <alkhizha@gmail.com>
2020-02-05 19:01:10 +0200

    Added CMakeProjectVS2

---
+++ CMakeProjectVS2/CMakeProjectVS2.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProjectVS2.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProjectVS2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMakeVS2!" << endl;
+	return 0;
+}
